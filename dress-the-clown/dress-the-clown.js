652e601ea0184d382d90e2abf122d93cb268c24b
Mave <chanzuidan@gmail.com>
2020-10-09 23:22:01 +1300

    2nd

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -3,3 +3,125 @@
 // -    -   -   -   -  //
 
 console.log("Dress The Clown!")
+
+
+
+document.onkeydown = checkKey;
+
+
+var clothingIndex=0;
+var headIndex=0;
+var bodyIndex=0;
+var shoesIndex=0;
+
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+
+      
+        // up arrow
+        if(clothingIndex<=1){
+          clothingIndex++;
+        }
+        if(clothingIndex==0){
+          document.getElementById('but').innerHTML='Dressing the head';
+
+        }
+        else if(clothingIndex==1){
+          document.getElementById('but').innerHTML='Dressing the body';
+
+        }
+        else{
+          document.getElementById('but').innerHTML='Dressing the feet';
+
+
+        }
+        
+       
+    }
+    else if (e.keyCode == '40') {
+
+      
+        // down arrow
+        if(clothingIndex>=1){
+          clothingIndex--;
+        }
+
+        if(clothingIndex==0){
+          document.getElementById('but').innerHTML='Dressing the head';
+
+        }
+        else if(clothingIndex==1){
+          document.getElementById('but').innerHTML='Dressing the body';
+
+        }
+        else{
+          document.getElementById('but').innerHTML='Dressing the feet';
+
+
+        }
+
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+
+       if(clothingIndex==0){
+        document.getElementById('head').src='images/head'+headIndex+'.png';
+        
+        if(headIndex>=1){
+            headIndex--;
+        }
+      }
+      else if(clothingIndex==1){
+
+        document.getElementById('body').src='images/body'+bodyIndex+'.png';
+      
+        if(bodyIndex>=1){
+            bodyIndex--;
+        }
+      }
+      else if(clothingIndex==2){
+
+        document.getElementById('shoes').src='images/shoes'+shoesIndex+'.png';
+      
+        if(shoesIndex>=1){
+            shoesIndex--
+        }
+      }
+     
+
+    }
+    else if (e.keyCode == '39') {
+      // right arrow
+
+      if(clothingIndex==0){
+        document.getElementById('head').src='images/head'+headIndex+'.png';
+      
+        if(headIndex<=4){
+            headIndex++
+        }
+      }
+      else if(clothingIndex==1){
+        document.getElementById('body').src='images/body'+bodyIndex+'.png';
+      
+        if(bodyIndex<=4){
+            bodyIndex++
+        }
+      }
+      else if(clothingIndex==2){
+        document.getElementById('shoes').src='images/shoes'+shoesIndex+'.png';
+      
+        if(shoesIndex<=4){
+            shoesIndex++
+        }
+      }
+      
+     
+       
+    }
+
+}
+      
--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,3 +3,48 @@
 // -    -   -   -   -  //
 
 console.log("Inflate The Unicorn!")
+
+var i1=0;
+var i2=0;
+var i3=0;
+
+document.getElementById('img1').onclick=function(){
+      if(i1==4){
+        alert('Unicorn number 1'+'says thank you');
+      }
+
+      if(i1<4){
+        document.getElementById('img1').src='images/unicorn-'+i1+'.png';
+        i1++;
+      }
+
+      
+}
+ 
+
+document.getElementById('img2').onclick=function(){
+      if(i2==4){
+        alert('Unicorn number 2'+' says thank you');
+    }
+
+
+  if(i2<4){
+    document.getElementById('img2').src='images/unicorn-'+i2+'.png';
+    i2++;
+  }
+
+  
+}
+
+document.getElementById('img3').onclick=function(){
+  if(i3==4){
+    alert('Unicorn number 3'+' says thank you');
+}
+
+  if(i3<4){
+    document.getElementById('img3').src='images/unicorn-'+i3+'.png';
+    i3++;
+  }
+
+  
+}
